2a747861b8cace7b76d6c6159c01a265f63a1d41
jon-hatfield <>
2024-01-30 15:21:57 +0000

    google analytics

--- docs/build-jekyll-prod.sh
+++ docs/build-jekyll-prod.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -rf docs/*
+echo "ga_tracking: $RAID_JEKYLL_GA" >> _config.yml
 JEKYLL_ENV=production bundle exec jekyll build --destination docs
+sed -i '' -e '$d' _config.yml
 touch docs/.nojekyll
